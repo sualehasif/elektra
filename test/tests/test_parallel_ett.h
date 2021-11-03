e345879238bb4faaa70b65a4988231d19a5e1bad
Tom Tseng <tom.hm.tseng@gmail.com>
2021-11-03 02:48:06 -0400

    tests: Combine test_parallel_ett and test_parallel_euler_tree
    It's confusing to have two test files for testing the same class, so
    let's just combine them.
    
    Also, I fixed the existing tests in test_parallel_ett --- there were two
    tests, but due to sharing data structures in their test fixture, the
    second test would fail.

--- test/tests/test_parallel_ett.h
+++ test/tests/test_parallel_ett.h
@@ -4,136 +4,184 @@
 
 #include <random>
 #include <string>
+#include <utility>
 
 #include "../../elektra/parallel_euler_tour_tree/euler_tour_tree.hpp"
 #include "../../elektra/utilities/simple_forest_connectivity.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 // TODO: parallelize the loops in this based on the original.
-
-constexpr int num_vertices{100};
-constexpr int link_attempts_per_round{200};
-constexpr int cut_ratio{3};
-constexpr int num_rounds{2};
 
 namespace elektra::testing {
 namespace {
 using EulerTourTree = parallel_euler_tour_tree::EulerTourTree;
 
-class ParallelETTTest : public ::testing::Test {
- protected:
-  ParallelETTTest() {
-    std::cout << "Parallel ETT: Testing";
-    rng.seed(0);
-  }
+using ::testing::IsEmpty;
+using ::testing::Pair;
+using ::testing::UnorderedElementsAre;
 
-  ~ParallelETTTest() override {}
-
-  std::mt19937 rng{};
-
-  std::uniform_int_distribution<std::mt19937::result_type> vert_dist{
-      0, num_vertices - 1};
-  std::uniform_int_distribution<std::mt19937::result_type> coin{0, 1};
-
-  SimpleForestConnectivity reference_solution{num_vertices};
-  EulerTourTree ett{num_vertices};
-  std::unordered_set<std::pair<int, int>, HashIntPairStruct> edges{};
-
-  parlay::sequence<std::pair<int, int>> ett_input =
-      parlay::sequence<std::pair<int, int>>{};
-
-  //   void CheckAllPairsConnectivity(
-  //       const SimpleForestConnectivity& reference_solution,
-  //       const EulerTourTree& ett) {
-  //     for (int u = 0; u < num_vertices; u++) {
-  //       for (int v = 0; v < num_vertices; v++) {
-  //         if (reference_solution.IsConnected(u, v) != ett.IsConnected(u, v))
-  //         {
-  //           std::cout << "Error: " << u << " " << v << std::endl;
-  //         }
-
-  //         assert(reference_solution.IsConnected(u, v) == ett.IsConnected(u,
-  //         v));
-  //       }
-  //     }
-  //   }
-};
-
-TEST_F(ParallelETTTest, SingleRoundLinkingElements) {
+TEST(ParallelEulerTourTreeTest, RandomLinksAndCuts) {
   // Generate `link_attempts_per_round` edges randomly, keeping each one that
   // doesn't add a cycle into the forest. Then call `BatchLink` on all of
   // them.
 
-  for (int j = 0; j < link_attempts_per_round; j++) {
-    const unsigned long u{vert_dist(rng)}, v{vert_dist(rng)};
-    if (!reference_solution.IsConnected(u, v)) {
-      reference_solution.Link(u, v);
-      edges.emplace(u, v);
-      ett_input.push_back(std::make_pair(u, v));
-    }
-  }
-  ett.BatchLink(ett_input);
+  constexpr int num_vertices{100};
+  constexpr int num_rounds{2};
+  constexpr int link_attempts_per_round{200};
+  constexpr int cut_ratio{3};
 
-  std::cout << "Edges: ";
-  for (auto& edge : edges) {
-    std::cout << edge.first << "<->" << edge.second << "  ||   ";
-  }
-  std::cout << std::endl;
-
-  for (int u = 0; u < num_vertices; u++) {
-    for (int v = 0; v < num_vertices; v++) {
-      if (reference_solution.IsConnected(u, v) != ett.IsConnected(u, v)) {
-        std::cout << "Error: Connected component of " << u << ": ";
-        for (auto& c : ett.ConnectedComponent(u)) {
-          std::cout << c << " ";
-        }
-        std::cout << std::endl;
-        std::cout << "Error: Connected component of " << v << ": ";
-        for (auto& c : ett.ConnectedComponent(v)) {
-          std::cout << c << " ";
-        }
-        std::cout << std::endl;
-      }
-
-      EXPECT_EQ(reference_solution.IsConnected(u, v), ett.IsConnected(u, v));
-    }
-  }
-}
-
-TEST_F(ParallelETTTest, MultipleRoundLinkingElements) {
-  // Empty ett_input
-  ett_input.assign(0, std::make_pair(0, 0));
-
-  EXPECT_EQ(ett_input.size(), 0);
+  EulerTourTree ett{num_vertices};
+  SimpleForestConnectivity reference_solution{num_vertices};
+  std::unordered_set<std::pair<int, int>, HashIntPairStruct> edges{};
+  std::mt19937 rng{0};
+  std::uniform_int_distribution<std::mt19937::result_type> vert_dist{0, num_vertices - 1};
+  std::uniform_int_distribution<std::mt19937::result_type> coin{0, 1};
 
   for (int i = 0; i < num_rounds; i++) {
-    // Generate `link_attempts_per_round` edges randomly, keeping each one that
-    // doesn't add a cycle into the forest. Then call `BatchLink` on all of
-    // them.
-
+    parlay::sequence<std::pair<int, int>> links = parlay::sequence<std::pair<int, int>>{};
     for (int j = 0; j < link_attempts_per_round; j++) {
       const unsigned long u{vert_dist(rng)}, v{vert_dist(rng)};
       if (!reference_solution.IsConnected(u, v)) {
         reference_solution.Link(u, v);
         edges.emplace(u, v);
-        ett_input.push_back(std::make_pair(u, v));
+        links.push_back(std::make_pair(u, v));
       }
     }
-    ett.BatchLink(ett_input);
+    ett.BatchLink(links);
 
     for (int u = 0; u < num_vertices; u++) {
       for (int v = 0; v < num_vertices; v++) {
-        if (reference_solution.IsConnected(u, v) != ett.IsConnected(u, v)) {
-          std::cout << "Error: " << u << " " << v << std::endl;
-        }
-
         EXPECT_EQ(reference_solution.IsConnected(u, v), ett.IsConnected(u, v));
       }
     }
 
-    std::cout << "Round " << i << ": " << ett_input.size()
-              << " edges inserted\n";
+    // Call `BatchCut` over each `cut_ratio`-th edge.
+    parlay::sequence<std::pair<int, int>> cuts = parlay::sequence<std::pair<int, int>>{};
+    int cnt{0};
+    for (auto e : edges) {
+      if (++cnt % cut_ratio == 0) {
+        cuts.push_back(e);
+      }
+    }
+    for (size_t j = 0; j < cuts.size(); j++) {
+      pair<int, int> cut{cuts[j]};
+      edges.erase(cut);
+      if (coin(rng) == 1) {
+        cuts[j] = make_pair(cut.second, cut.first);
+      }
+      reference_solution.Cut(cut.first, cut.second);
+    }
+    ett.BatchCut(cuts);
+
+    for (int u = 0; u < num_vertices; u++) {
+      for (int v = 0; v < num_vertices; v++) {
+        EXPECT_EQ(reference_solution.IsConnected(u, v), ett.IsConnected(u, v));
+      }
+    }
   }
+}
+
+TEST(ParallelEulerTourTreeTest, IsDisconnectedInitially) {
+  const EulerTourTree ett = EulerTourTree{3};
+  EXPECT_FALSE(ett.IsConnected(0, 1));
+  EXPECT_FALSE(ett.IsConnected(0, 2));
+  EXPECT_FALSE(ett.IsConnected(1, 2));
+}
+
+// Create a small line graph and test that it's connected. Then cut it in half
+// and test that it's disconnected.
+TEST(ParallelEulerTourTreeTest, ShortLineGraph) {
+  const int n = 7;
+  EulerTourTree ett = EulerTourTree{n};
+
+  for (int i = 0; i < n - 1; i++) {
+    ett.Link(i, i + 1);
+  }
+  EXPECT_TRUE(ett.IsConnected(0, n - 1));
+
+  ett.Cut(n / 2, n / 2 + 1);
+  EXPECT_TRUE(ett.IsConnected(0, n / 2));
+  EXPECT_TRUE(ett.IsConnected(n / 2 + 1, n - 1));
+  EXPECT_FALSE(ett.IsConnected(0, n - 1));
+}
+
+// Adds edges creating two large star graphs linked by an edge, then removes the
+// star graph edges.
+//
+// This larger test is to check that parallel link and parallel cut works as
+// expected.
+// TODO(tomtseng): The ETT operates on batches sequentially if the batch is
+// smaller than some size S. But we may want to tune S, and we don't want that
+// to affect this test. We should let this test manually override S by exposing
+// the S as a optionally tunable parameter.
+TEST(ParallelEulerTourTreeTest, BigStarGraphs) {
+  const int n = 200;
+  ASSERT_EQ(n % 2, 0);
+  EulerTourTree ett = EulerTourTree{n};
+
+  auto links = parlay::sequence<std::pair<int, int>>::uninitialized(n - 1);
+  auto cuts = parlay::sequence<std::pair<int, int>>::uninitialized(n - 2);
+  // The centers of the two stars are vertices n / 2 - 1 and n / 2.
+  for (int i = 0; i < n / 2 - 1; i++) {
+    links[i] = make_pair(i, n / 2 - 1);
+    cuts[i] = make_pair(i, n / 2 - 1);
+  }
+  for (int i = n / 2 + 1; i < n; i++) {
+    links[i - 2] = make_pair(n / 2, i);
+    cuts[i - 2] = make_pair(n / 2, i);
+  }
+  links[n - 2] = make_pair(n / 2 - 1, n / 2);
+
+  ett.BatchLink(links);
+  EXPECT_TRUE(ett.IsConnected(0, 1));
+  EXPECT_TRUE(ett.IsConnected(0, n / 2 - 1));
+  EXPECT_TRUE(ett.IsConnected(0, n - 1));
+  EXPECT_TRUE(ett.IsConnected(n / 2 - 1, n / 2));
+
+  ett.BatchCut(cuts);
+  EXPECT_FALSE(ett.IsConnected(0, 1));
+  EXPECT_FALSE(ett.IsConnected(0, n / 2 - 1));
+  EXPECT_FALSE(ett.IsConnected(0, n - 1));
+  EXPECT_TRUE(ett.IsConnected(n / 2 - 1, n / 2));
+}
+
+TEST(ParallelEulerTourTreeTest, ComponentVerticesAndEdges) {
+  EulerTourTree ett = EulerTourTree{6};
+  ett.Link(0, 2);
+  ett.Link(1, 2);
+  ett.Link(3, 5);
+
+  EXPECT_THAT(ett.ComponentVertices(0), UnorderedElementsAre(0, 1, 2));
+  EXPECT_THAT(ett.ComponentEdges(0), UnorderedElementsAre(Pair(0, 2), Pair(1, 2)));
+  EXPECT_THAT(ett.ComponentVertices(3), UnorderedElementsAre(3, 5));
+  EXPECT_THAT(ett.ComponentEdges(3), UnorderedElementsAre(Pair(3, 5)));
+  EXPECT_THAT(ett.ComponentVertices(4), UnorderedElementsAre(4));
+  EXPECT_THAT(ett.ComponentEdges(4), IsEmpty());
+}
+
+TEST(ParallelEulerTourTreeTest, GetRepresentative) {
+  const int n = 8;
+  EulerTourTree ett = EulerTourTree{n};
+
+  ett.Link(0, 1);
+  ett.Link(0, 2);
+  ett.Link(1, 3);
+  ett.Link(1, 4);
+  ett.Link(6, 7);
+
+  std::vector<int> reps(n);
+  for (int i = 0; i < n; i++) {
+    reps[i] = ett.GetRepresentative(i);
+  }
+  EXPECT_EQ(reps[0], reps[1]);
+  EXPECT_EQ(reps[0], reps[2]);
+  EXPECT_EQ(reps[0], reps[3]);
+  EXPECT_EQ(reps[0], reps[4]);
+  EXPECT_NE(reps[0], reps[5]);
+  EXPECT_NE(reps[0], reps[6]);
+  EXPECT_NE(reps[5], reps[6]);
+  EXPECT_EQ(reps[6], reps[7]);
 }
 
 }  // namespace
