0e2d003fda82d7c3d8e9f0687fee4585599eaf69
Tom Tseng <tom.hm.tseng@gmail.com>
2021-11-03 02:50:40 -0400

    test: Rename test_parallel_ett -> test_parallel_euler_tour_tree
    For clarity.

--- test/elektra_test.cpp
+++ test/elektra_test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "tests/test_augmented_skip_list.h"
 #include "tests/test_parallel_batch_connected.h"
-#include "tests/test_parallel_ett.h"
+#include "tests/test_parallel_euler_tour_tree.h"
 #include "tests/test_parallel_skip_list.h"
 
 int main(int argc, char **argv) {
