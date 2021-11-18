47658cd2552fb7b977ac802db10e4797f851bfa1
Tom Tseng <tom.hm.tseng@gmail.com>
2021-11-18 17:38:50 -0500

    euler_tour_tree: Split ETT, EdgeMap, Element into separate files

---
+++ elektra/parallel_euler_tour_tree/edge_map.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <parlay/alloc.h>
+#include <parlay/parallel.h>
+#include <parlay/primitives.h>
+
+#include "concurrentMap.h"
+#include "element.hpp"
+#include "hash_pair.h"
+
+namespace parallel_euler_tour_tree {
+
+namespace _internal {
+
+// The element allocator is used to allocate and deallocate elements.
+// It is used to allocate the elements in the skip list.
+// This is borrowed from parlay's type allocator.
+using ElementAllocator = parlay::type_allocator<Element>;
+
+// Used in Euler tour tree for mapping directed edges (pairs of ints) to the
+// sequence element in the Euler tour representing the edge.
+//
+// Only one of (u, v) and (v, u) should be added to the map; we can find the
+// other edge using the `twin_` pointer in `Element`.
+class EdgeMap {
+ public:
+  EdgeMap() = delete;
+  explicit EdgeMap(int num_vertices);
+  ~EdgeMap();
+
+  bool Insert(int u, int v, Element* edge);
+  bool Delete(int u, int v);
+  Element* Find(int u, int v);
+
+  int IsEmpty() const;
+
+  // Deallocate all elements held in the map. This assumes that all elements
+  // in the map were allocated through `allocator`.
+  void FreeElements();
+
+  // Returns an estimate of the amount of memory in bytes this element occupies
+  // beyond what's captured by sizeof(EdgeMap). This includes the amount of
+  // memory occupied by the `Element`s allocated and inserted as values into the
+  // map.
+  size_t AllocatedMemorySize() const;
+
+ private:
+  concurrent_map::concurrentHT<std::pair<int, int>, Element*, HashIntPairStruct>
+      map_;
+};
+
+EdgeMap::EdgeMap(int num_vertices)
+    : map_{nullptr, static_cast<size_t>(num_vertices - 1),
+           std::make_pair(-1, -1), std::make_pair(-2, -2)} {}
+
+EdgeMap::~EdgeMap() { map_.del(); }
+
+// Check whether the hash table is empty.
+int EdgeMap::IsEmpty() const { return map_.n_elms == 0; }
+
+bool EdgeMap::Insert(int u, int v, Element* edge) {
+  if (u > v) {
+    std::swap(u, v);
+    edge = edge->twin_;
+  }
+  return map_.insert(make_pair(u, v), edge);
+}
+
+bool EdgeMap::Delete(int u, int v) {
+  if (u > v) {
+    std::swap(u, v);
+  }
+  return map_.deleteVal(make_pair(u, v));
+}
+
+Element* EdgeMap::Find(int u, int v) {
+  if (u > v) {
+    Element* vu{*map_.find(make_pair(v, u))};
+    return vu == nullptr ? nullptr : vu->twin_;
+  } else {
+    return *map_.find(make_pair(u, v));
+  }
+}
+
+// Assumes that all elements were allocator using ElementAllocator.
+
+void EdgeMap::FreeElements() {
+  parlay::parallel_for(0, map_.capacity, [&](size_t i) {
+    auto kv{map_.table[i]};
+    auto key{get<0>(kv)};
+    if (key != map_.empty_key && key != map_.tombstone) {
+      Element* element{get<1>(kv)};
+      element->twin_->~Element();
+      ElementAllocator::free(element->twin_);
+      element->~Element();
+      ElementAllocator::free(element);
+    }
+  });
+}
+
+size_t EdgeMap::AllocatedMemorySize() const {
+  return sizeof(map_.table[0]) * map_.capacity +
+    parlay::reduce(
+      parlay::map(map_.entries(), [&](std::tuple<std::pair<int, int>, Element*> kv) {
+        const Element* elem{std::get<1>(kv)};
+        // Count both elem and elem->twin_ since only one appears in map_.
+        return 2 * sizeof(*elem) + elem->AllocatedMemorySize() + elem->twin_->AllocatedMemorySize();
+      })
+    );
+}
+
+}  // namespace _internal
+}  // namespace parallel_euler_tour_tree
---
+++ elektra/parallel_euler_tour_tree/element.hpp
@@ -0,0 +1,201 @@
+#pragma once
+
+#include <parlay/monoid.h>
+#include <parlay/monoid.h>
+#include <parlay/sequence.h>
+
+#include <limits>
+#include <utility>
+
+#include "parallel_skip_list/augmented_skip_list.h"
+
+namespace parallel_euler_tour_tree {
+namespace _internal {
+
+class Element : public parallel_skip_list::AugmentedElementBase<Element, parlay::addm<int>> {
+  using Base = parallel_skip_list::AugmentedElementBase<Element, parlay::addm<int>>;
+
+ public:
+  explicit Element(size_t random_int, std::pair<int, int> id)
+    // Augment with the function "count the number of edges (u, v) such that u <
+    // v in this list".
+      : Base{random_int, id.first < id.second}
+      , id_{id} {}
+
+  // Returns a representative vertex from the sequence the element lives in.
+  // Whereas `FindRepresentative()` returns a representative element that might
+  // be an edge, this function returns a vertex ID. For implementation
+  // simplicity, this function also assumes that the sequence is circular (as is
+  // the case for a sequence representing an ETT component that is not currently
+  // performing a join or split).
+  int FindRepresentativeVertex() const;
+  // Get all edges {u, v} in the sequence that contains this element, assuming
+  // that the sequence represents an ETT component.
+  parlay::sequence<std::pair<int, int>> GetEdges();
+  // Get the number of vertices in the sequence that contains this element.
+  size_t GetComponentSize();
+
+  // Returns an estimate of the amount of memory in bytes this element occupies
+  // beyond what's captured by sizeof(Element).
+  size_t AllocatedMemorySize() const;
+
+  // If this element represents a vertex v, then id == (v, v). Otherwise if
+  // this element represents a directed edge (u, v), then id == (u,v).
+  std::pair<int, int> id_;
+  // If this element represents edge (u, v), `twin` should point towards (v, u).
+  Element* twin_{nullptr};
+  // When batch splitting, we mark this as `true` for an edge that we will
+  // splice out in the current round of recursion.
+  bool split_mark_{false};
+
+ private:
+  friend Base;
+
+  // Gets edges held in descendants of this element and writes them into
+  // the sequence starting at the offset. `values_` needs to be up to date.
+  void GetEdgesBelow(parlay::sequence<std::pair<int, int>>* s, int level, int offset) const;
+  // Helper function for GetEdgesBelow().
+  static void GetEdgesBelowImpl(
+      parlay::sequence<std::pair<int, int>>* s,
+      int level,
+      int offset,
+      const Element* curr,
+      bool is_loop_start = true);
+  // Helper function for GetEdges().
+  static void GetEdgesImpl(
+      parlay::sequence<std::pair<int, int>>* s,
+      const Element* start_element,
+      const Element* curr,
+      int offset = 0,
+      bool is_loop_start = true);
+};
+
+void Element::GetEdgesBelowImpl(
+    parlay::sequence<std::pair<int, int>>* s,
+    int level,
+    int offset,
+    const Element* curr,
+    bool is_loop_start) {
+  if (is_loop_start || curr->height_ < level + 1) {
+    parlay::par_do(
+      [&]() { curr->GetEdgesBelow(s, level, offset); },
+      [&]() { GetEdgesBelowImpl(s, level, offset + curr->values_[level], curr->neighbors_[level].next, false); }
+    );
+  }
+}
+
+void Element::GetEdgesImpl(
+    parlay::sequence<std::pair<int, int>>* s,
+    const Element* start_element,
+    const Element* curr,
+    int offset,
+    bool is_loop_start) {
+  if (is_loop_start || curr != start_element) {
+    const int level = curr->height_ - 1;
+    parlay::par_do(
+      [&]() { curr->GetEdgesBelow(s, level, offset); },
+      [&]() { GetEdgesImpl(s, start_element, curr->neighbors_[level].next, offset + curr->values_[level], false); }
+    );
+  }
+}
+
+void Element::GetEdgesBelow(parlay::sequence<std::pair<int, int>>* s, int level, int offset) const {
+  if (level == 0) {
+    if (values_[0]) {
+      (*s)[offset] = id_;
+    }
+    return;
+  }
+  if (values_[level] == 0) {
+    return;
+  }
+
+  const Element* curr{this};
+  if (level <= 6) {
+    // run sequentially once we're near the bottom of the list and not doing as
+    // much work per thread
+    do {
+      curr->GetEdgesBelow(s, level - 1, offset);
+      offset += curr->values_[level - 1];
+      curr = curr->neighbors_[level - 1].next;
+    } while (curr->height_ < level + 1);
+  } else {  // run in parallel
+    GetEdgesBelowImpl(s, level - 1, offset, this);
+  }
+}
+
+parlay::sequence<std::pair<int, int>> Element::GetEdges() {
+  Element* const top_element{FindRepresentative()};
+  const int level = top_element->height_ - 1;
+
+  size_t num_edges{0};
+  {
+    Element* curr = top_element;
+    do {
+      num_edges += curr->values_[level];
+      curr = curr->neighbors_[level].next;
+    } while (curr != top_element);
+  }
+
+  parlay::sequence<std::pair<int, int>> edges(num_edges);
+  GetEdgesImpl(&edges, top_element, top_element);
+  return edges;
+}
+
+size_t Element::GetComponentSize() {
+  Element* const top_element{FindRepresentative()};
+  const int level = top_element->height_ - 1;
+
+  size_t num_edges{0};
+  Element* curr = top_element;
+  do {
+    num_edges += curr->values_[level];
+    curr = curr->neighbors_[level].next;
+  } while (curr != top_element);
+
+  const size_t num_vertices = num_edges + 1;
+  return num_vertices;
+}
+
+size_t Element::AllocatedMemorySize() const {
+  // Estimate size of neighbors_ and values_, knowing that they're allocated
+  // using concurrent_array_allocator.
+  return (sizeof(neighbors_[0]) + sizeof(values_[0])) * (1 << parlay::log2_up(height_));
+}
+
+int Element::FindRepresentativeVertex() const {
+  const Element* current_element{this};
+  const Element* seen_element{nullptr};
+  int current_level{current_element->height_ - 1};
+
+  // walk up while moving forward to find top level
+  while (seen_element != current_element) {
+    if (seen_element == nullptr) {
+      seen_element = current_element;
+    }
+    current_element = current_element->neighbors_[current_level].next;
+    const int top_level{current_element->height_ - 1};
+    if (current_level < top_level) {
+      current_level = top_level;
+      seen_element = nullptr;
+    }
+  }
+
+  // look for minimum ID vertex in top level, or try again at lower levels if no
+  // vertex is found
+  int min_vertex{std::numeric_limits<int>::max()};
+  while (current_level >= 0 && min_vertex == std::numeric_limits<int>::max()) {
+    do {
+      const std::pair<int, int>& id{current_element->id_};
+      if (id.first == id.second && id.first < min_vertex) {
+        min_vertex = id.first;
+      }
+      current_element = current_element->neighbors_[current_level].next;
+    } while (current_element != seen_element);
+    current_level--;
+  }
+  return min_vertex == std::numeric_limits<int>::max() ? -1 : min_vertex;
+}
+
+}  // namespace _internal
+}  // namespace parallel_euler_tour_tree
