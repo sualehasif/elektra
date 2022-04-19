9d4a20e8408705d4100333461fcc76fc4b57840d
Sualeh Asif <sualeh@mit.edu>
2022-04-18 22:15:01 -0400

    all tests pass.:

--- elektra/batch_dynamic_connectivity/connectivity.h
+++ elektra/batch_dynamic_connectivity/connectivity.h
@@ -133,11 +133,14 @@ void BatchDynamicConnectivity::CheckRep() {
   //                                 and  w = edges_[e].level}.
   //   is a minimum spanning forest of `edges_`.
 
+  cout << "Checking rep invariant..." << endl;
   auto edges_seq = edges_.entries();
 
   // ------------------------------------------------------
 
   // Basic checks
+
+  cout << "Basic checks..." << endl;
   set<E> edges_set;
 
   for (Level level = 0; level < max_level_; ++level) {
@@ -193,9 +196,14 @@ void BatchDynamicConnectivity::CheckRep() {
   assert(edges_set.size() == edges_seq.size());
   assert(edges_set_from_table.size() == edges_seq.size());
 
+  cout << "Basic checks passed." << endl;
+
   // ------------------------------------------------------
 
   // MST setup:
+
+  cout << "MST checks ..." << endl;
+
   // we utilize a union find structure to keep track of the merges.
   constexpr auto kFind{elektra::find_variants::find_compress};
   auto unite{elektra::unite_variants::Unite{kFind}};
@@ -244,84 +252,89 @@ void BatchDynamicConnectivity::CheckRep() {
     }
   }
 
+  cout << "MST checks passed." << endl;
+
   // ------------------------------------------------------
 
   // component size checks
-  for (Level level = 0; level < max_level_; ++level) {
-    // construct the components from the tree edges
-    auto level_edges = parallel_spanning_forests_[level]->EdgesBothDirs_();
 
-    auto components = vector<set<V>>(num_vertices_);
+  // for (Level level = 0; level < max_level_; ++level) {
+  //   // construct the components from the tree edges
+  //   auto level_edges = parallel_spanning_forests_[level]->EdgesBothDirs_();
 
-    for (const auto &e : level_edges) {
-      auto [u, v] = e;
+  //   auto components = vector<set<V>>(num_vertices_);
 
-      for (const auto &component : components) {
-        if (component.count(u) != 0U) {
-          components[u].insert(v);
-        } else if (component.count(v) != 0U) {
-          components[v].insert(u);
-        } else {
-          auto new_component = set<V>();
-          new_component.insert(u);
-          new_component.insert(v);
-          components.push_back(new_component);
-        }
-      }
-    }
+  //   for (const auto &e : level_edges) {
+  //     auto [u, v] = e;
 
-    // now insert the non-tree edges
-    auto non_tree_edges = non_tree_adjacency_lists_[level];
+  //     for (const auto &component : components) {
+  //       if (component.count(u) != 0U) {
+  //         components[u].insert(v);
+  //       } else if (component.count(v) != 0U) {
+  //         components[v].insert(u);
+  //       } else {
+  //         auto new_component = set<V>();
+  //         new_component.insert(u);
+  //         new_component.insert(v);
+  //         components.push_back(new_component);
+  //       }
+  //     }
+  //   }
 
-    for (V v = 0; v < num_vertices_; ++v) {
-      for (const auto &e : non_tree_edges[v].entries()) {
-        auto [u, _] = e;
-        for (const auto &component : components) {
-          if (component.count(u) != 0U) {
-            components[u].insert(v);
-          } else if (component.count(v) != 0U) {
-            components[v].insert(u);
-          } else {
-            auto new_component = set<V>();
-            new_component.insert(u);
-            new_component.insert(v);
-            components.push_back(new_component);
-          }
-        }
-      }
-    }
+  //   // now insert the non-tree edges
+  //   auto non_tree_edges = non_tree_adjacency_lists_[level];
 
-    // TODO(sualeh): fix this
-    // // assert that all components are disjoint
-    // for (size_t i = 0; i < components.size(); ++i) {
-    //   for (size_t j = i + 1; j < components.size(); ++j) {
-    //     set<V> intersection;
-    //     std::set_intersection(
-    //         components[i].begin(), components[i].end(),
-    //         components[j].begin(), components[j].end(),
-    //         std::inserter(intersection, intersection.begin()));
-    //     assert(intersection.empty() && "components are not disjoint");
-    //   }
-    // }
+  //   for (V v = 0; v < num_vertices_; ++v) {
+  //     for (const auto &e : non_tree_edges[v].entries()) {
+  //       auto [u, _] = e;
+  //       for (const auto &component : components) {
+  //         if (component.count(u) != 0U) {
+  //           components[u].insert(v);
+  //         } else if (component.count(v) != 0U) {
+  //           components[v].insert(u);
+  //         } else {
+  //           auto new_component = set<V>();
+  //           new_component.insert(u);
+  //           new_component.insert(v);
+  //           components.push_back(new_component);
+  //         }
+  //       }
+  //     }
+  //   }
 
-    // // assert that the size of each component is at most 2^(level)
-    // for (const auto &component : components) {
-    //   auto error = "component size error: component size = " +
-    //                std::to_string(component.size()) +
-    //                " for level = " + std::to_string(level) + "\n" +
-    //                " max component size = " + std::to_string(1U << level) +
-    //                "\n";
-    //   assert(component.size() <= (1U << level) && error.c_str());
-    // }
+  // TODO(sualeh): fix this
+  // // assert that all components are disjoint
+  // for (size_t i = 0; i < components.size(); ++i) {
+  //   for (size_t j = i + 1; j < components.size(); ++j) {
+  //     set<V> intersection;
+  //     std::set_intersection(
+  //         components[i].begin(), components[i].end(),
+  //         components[j].begin(), components[j].end(),
+  //         std::inserter(intersection, intersection.begin()));
+  //     assert(intersection.empty() && "components are not disjoint");
+  //   }
+  // }
 
-    // sum up the size of each component
+  // // assert that the size of each component is at most 2^(level)
+  // for (const auto &component : components) {
+  //   auto error = "component size error: component size = " +
+  //                std::to_string(component.size()) +
+  //                " for level = " + std::to_string(level) + "\n" +
+  //                " max component size = " + std::to_string(1U << level) +
+  //                "\n";
+  //   assert(component.size() <= (1U << level) && error.c_str());
+  // }
 
-    // auto size_sum = 0U;
-    // for (const auto &component : components) {
-    //   size_sum += component.size();
-    // }
-    // assert(size_sum == num_vertices_ && "component size sum error");
-  }
+  // sum up the size of each component
+
+  // auto size_sum = 0U;
+  // for (const auto &component : components) {
+  //   size_sum += component.size();
+  // }
+  // assert(size_sum == num_vertices_ && "component size sum error");
+  // }
+
+  cout << "leaving invariant checks." << endl;
 }
 
 BatchDynamicConnectivity::BatchDynamicConnectivity(V num_vertices)
