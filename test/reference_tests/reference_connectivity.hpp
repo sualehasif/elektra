642a2e54e97ce855d62fc958bc700df7be1e8812
Sualeh Asif <sualeh@mit.edu>
2022-05-01 00:34:20 -0400

    removing self loops and the like for reference testing

--- test/reference_tests/reference_connectivity.hpp
+++ test/reference_tests/reference_connectivity.hpp
@@ -10,6 +10,19 @@
 #include "../../elektra/batch_dynamic_connectivity/dynamic_connectivity.h"
 #include "gtest/gtest.h"
 
+auto get_benchmark_path(const std::string& name) -> std::string {
+  // get path using "git rev-parse --show-toplevel"
+  char cwd[PATH_MAX];
+  getcwd(cwd, sizeof(cwd));
+
+  // find where "elektra" lies
+  std::string path(cwd);
+  size_t pos = path.find("elektra");
+  path = path.substr(0, pos) + "elektra/";
+
+  return path + "benchmarks/graphs/" + name;
+}
+
 namespace elektra::testing {
 namespace {
 using std::make_pair;
@@ -29,12 +42,12 @@ class ReferenceConnectivityTest : public ::testing::Test {
 };
 
 TEST_F(ReferenceConnectivityTest, SimpleGraph) {
-  const std::string filename = "../../benchmarks/graphs/email-Eu-core.txt";
+  const auto filename = get_benchmark_path("email-Eu-core.txt");
   auto vertex_and_edge_list = io::read_unweighted_edge_list(filename.c_str());
 
   const int num_vertices = vertex_and_edge_list.first;
-  const auto edges = vertex_and_edge_list.second;
-  const int num_edges = edges.size();
+  const auto edges_base = vertex_and_edge_list.second;
+  const int num_edges = edges_base.size();
 
   std::cout << "num_vertices: " << num_vertices << std::endl;
   std::cout << "num_edges: " << num_edges << std::endl;
@@ -42,12 +55,22 @@ TEST_F(ReferenceConnectivityTest, SimpleGraph) {
   DynamicConnectivity reference(num_vertices);
   BatchDynamicConnectivity batch_connect(num_vertices);
 
-  // for (const auto &edge : edges) {
-  //   const auto e = UndirectedEdgeR(edge.first, edge.second);
-  //   reference.AddEdge(e);
-  // }
+  // remove self-loops
+  auto edges = parlay::sequence<E>::from_function(num_edges, [&](size_t i) {
+    if (edges_base[i].first < edges_base[i].second) {
+      return E(edges_base[i].first, edges_base[i].second);
+    }
+    return E(UINT32_MAX - 1, UINT32_MAX - 1);
+  });
+  edges = parlay::filter(edges,
+                         [&](const E& e) { return e.first != UINT32_MAX - 1; });
 
-  // batch_connect.BatchAddEdges(edges);
+  for (const auto& edge : edges) {
+    const auto e = UndirectedEdgeR(edge.first, edge.second);
+    reference.AddEdge(e);
+  }
+
+  batch_connect.BatchAddEdges(edges_base);
 
   // // generate random queries
   // std::uniform_int_distribution<int> dist(0, num_vertices - 1);
